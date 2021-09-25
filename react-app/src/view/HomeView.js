357e53caf9058629961e7eb2e146d56c50c04da7
myrlejon <60348075+myrlejon@users.noreply.github.com>
2021-09-25 13:22:54 +0200

    Update HomeView.js

--- react-app/src/view/HomeView.js
+++ react-app/src/view/HomeView.js
@@ -11,17 +11,40 @@ export const HomeView = () => {
             .catch((error) => console.log(error))
     }
 
+    
+// Gjorde om arrayen så att den returnerar med .map() funktionen
+// La till console.log(map), ett API anropp görs varje gång en bokstav läggs till
+
 const displayData = () => {
+
     if(data) {
-        return <div>
-            <h3>name: {data.name}</h3>
-            <h3>id: {data.id}</h3>
-            <h3>weight: {data.weight}</h3>
-            <h3>height: {data.height}</h3>
-            <h3>type: {data.types[0].type.name}</h3>
+        const array = [data.sprites.front_default, data.name, data.id, data.weight, data.height, data.types[0].type.name]
+        const map = array.map(x => x)
+        console.log(map)
+
+        return (
+            <ul>
+                <img src={array[0]} />
+                {array.map((element, i) =>
+                    <li key={i}>{element}</li>
+                )}
+            </ul>
+        )
+        
+        // Kommenterade bort det visuellt snyggare sättet att returnera en array eftersom uppgiften handlar om funktionalitet.
+
+        /*<div>
+            <h3>name: {array[1]}</h3>
+            <h3>id: {array[2]}</h3>
+            <h3>weight: {array[3]}</h3>
+            <h3>height: {array[4]}</h3>
+            <h3>type: {array[5]}</h3>
         </div>
+        */
     }
 }
+
+
 
     return (
         <div>
