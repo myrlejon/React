52b85df172b8f4a57f67f61882b5046ddda0f1b3
myrlejon <60348075+myrlejon@users.noreply.github.com>
2021-09-29 12:51:43 +0200

    Added some hooks

--- react-app/src/view/ProfileView.js
+++ react-app/src/view/ProfileView.js
@@ -1,7 +1,21 @@
+import { useHistory } from "react-router-dom"
+import { useState } from "react";
+import RoutingPath from "../routes/RoutingPath";
+
 export const ProfileView = () => {
+    const [count, setCount] = useState();
+    const history = useHistory()
+
+    const sendValue = () => {
+        setCount(count + 1)
+        history.push(RoutingPath.settingsView, [count])
+    }
+
     return (
         <div>
-            <h1>This is the ProfileView</h1>
+            <span>Set value: </span><input onChange={event => setCount(event.target.value)}/> <br/>
+            <h1>{count}</h1>
+            <button onClick={() => sendValue()}>Click to send value</button>
         </div>
     )
 }
--- react-app/src/view/SettingsView.js
+++ react-app/src/view/SettingsView.js
@@ -1,7 +1,12 @@
+import { useLocation } from "react-router-dom"
+import { useState, useEffect } from "react"
+import RoutingPath from "../routes/RoutingPath"
+
 export const SettingsView = () => {
+    let location = useLocation();
     return (
         <div>
-            <h1>This is the SettingsView</h1>
+            <h1>{location.state}</h1>
         </div>
     )
 }
